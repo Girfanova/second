e6fd44b4f3c9235b113d2975f7290d1273ea25b2
Girfanova Yuliya <Yuliyagiryul@gmail.com>
2022-09-20 02:39:36 +0500

    форма оранж и длинная

--- MyNotepad/MyNotepad/Form1.Designer.cs
+++ MyNotepad/MyNotepad/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(312, 187);
+            this.button1.Location = new System.Drawing.Point(430, 196);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(179, 60);
             this.button1.TabIndex = 0;
@@ -45,8 +45,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Yellow;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(1036, 450);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
